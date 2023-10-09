fd13ebbe7acf7c50b3a3d8f8f06f08f54a206c4c
Mikhail <aksenov.m87@yandex.ru>
2023-10-10 00:11:26 +0300

    feat: сверстать страницы 404, 500

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import Login from "./components/Login";
 // import Sidebar from "./components/Sidebar";
+// import PageNotFound from "./components/PageNotFound";
+// import Error500Page from "./components/Error500Page";
 
 const AppWrapper = styled.div`
   background: #fff;
   height: 100vh;
   width: 100%;
+  display: flex;
+  flex-direction: row;
 `;
 
 function App() {
